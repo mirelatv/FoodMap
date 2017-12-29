05b14c12e8123fdc3b5da33751f8982170ad7ebb
mirelatv <noelia.mirela@gmail.com>
2017-12-29 07:16:21 -0500

    Avance buscador al 60%

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/* Splash*/
+$(document).ready(function(){
+
+$('.initial').fadeOut(3000);
+
+})
+
+/* efecto de  cada pagina  visitada*/
+
+$( "button" ).click(function() {
+    $( "p" ).show( "slow" );
+  });
+
+/* Bucador*/
+  
+var buscador = $("#table").DataTable();
+
+$("#input-search").keyup(function(){
+    
+buscador.search($(this).val()).draw();
+    
+    if ($("#input-search").val() == ""){
+        $(".content-search").fadeOut(300);
+    }else{
+        $(".content-search").fadeIn(300);
+    }
+})
