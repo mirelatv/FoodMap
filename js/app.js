51f5803f7bfdcd45e6a095d86815fea0ff8db2a6
mirelatv <noelia.mirela@gmail.com>
2017-12-30 19:52:58 -0500

    cuarto avance

--- js/app.js
+++ js/app.js
@@ -7,33 +7,89 @@ $('.initial').fadeOut(5000);
 
 /* efecto de  cada pagina  visitada*/
 
-$( "button" ).click(function() {
-    $( "p" ).show( "slow" );
+$( '.call' ).click(function() {
+    $( '.telephone' ).show( 'slow');
     
   });
-
+  
 /* Bucador*/
   
-var buscador = $("#table").DataTable();
+var buscador = $('#table').DataTable();
 
-$("#input-search").keyup(function(){
+   $('#input-search').keyup(function(){
     
 buscador.search($(this).val()).draw();
     
-    if ($("#input-search").val() == ""){
-        $(".content-search").fadeOut(300);
+    if ($('#input-search').val() == ""){
+        $('.content-search').fadeOut(300);
     }else{
-        $(".content-search").fadeIn(300);
+        $('.content-search').fadeIn(300);
     }
 })
 
 /*mouseOver*/
 
-$(document).ready(function(){
-    $(".image").mouseover(function(){
-        $(".image").css("background", "rgba(0, 0, 0, 0.336)");
+
+    $(".first").mouseover(function(){
+        $(".first").css('background', 'rgba(0, 0, 0, 0.336)');
     });
-    $(".image").mouseout(function(){
-        $(".image").css("background","transparent");
+    $(".first").mouseout(function(){
+        $(".first").css('background','transparent');
+        
     });
-});
+
+
+    $('.second').mouseover(function(){
+        $(".second").css('background', 'rgba(0, 0, 0, 0.336)');
+    });
+    $('.second').mouseout(function(){
+        $(".second").css('background','transparent');
+     
+    });
+
+    $('.third').mouseover(function(){
+        $(".third").css('background', 'rgba(0, 0, 0, 0.336)');
+    });
+    $('.third').mouseout(function(){
+        $('.third').css('background','transparent');
+     
+    });
+
+    $('.fourth').mouseover(function(){
+        $('.fourth').css('background', 'rgba(0, 0, 0, 0.336)');
+    });
+    $('.fourth').mouseout(function(){
+        $('.fourth').css('background','transparent');
+     
+    });
+
+    $('.fifth').mouseover(function(){
+        $('.fifth').css('background', 'rgba(0, 0, 0, 0.336)');
+    });
+    $('.fifth').mouseout(function(){
+        $('.fifth').css('background','transparent');
+     
+    });
+
+    $('.sixth').mouseover(function(){
+        $('.sixth').css('background', 'rgba(0, 0, 0, 0.336)');
+    });
+    $('.sixth').mouseout(function(){
+        $('.sixth').css('background','transparent');
+     
+    });
+/* reemplazo de Info en Modal
+
+    modal-title
+     
+    direction
+    telephone 
+
+
+
+
+*/
+    
+
+
+
